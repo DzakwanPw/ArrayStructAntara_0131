8ee06f4643e6f98ce1fc1f78a3c735fec4a93888
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-07-23 14:30:23 +0700

    menambahkan tampilan data setelah pengisian data struct Orang

--- struck.cpp
+++ struck.cpp
@@ -20,4 +20,9 @@ int main()
     cout << "isikan data umur : ";
     cin >> mhs.umur;
     cout << endl;
+
+    cout << "Data Tampil" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
 }
