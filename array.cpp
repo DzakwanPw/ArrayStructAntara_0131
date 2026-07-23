22ef3e0b987fe63b7a4d649fa7a65935f41cea54
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-07-23 14:03:48 +0700

    array initialization syntax dan nambahin main function

--- array.cpp
+++ array.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    //Deklarasi Array disertai inisialisasi
+    int dataku[5] = [10, 20, 30, 40, 50];
+    //Deklarasi Tanpa inisialisasi
+    int datamu[5];
+}
