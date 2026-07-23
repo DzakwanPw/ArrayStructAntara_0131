7491fb99f128651bd4c7a29707d02064f238e2fa
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-07-23 14:31:54 +0700

    memperbaiki sintaksis inisialisasi array dataku di file array.cpp

--- array.cpp
+++ array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     //Deklarasi Array disertai inisialisasi
-    int dataku[5] = [10, 20, 30, 40, 50];
+    int dataku[5] = {10, 20, 30, 40, 50};
     //Deklarasi Tanpa inisialisasi
     int datamu[5];
 
