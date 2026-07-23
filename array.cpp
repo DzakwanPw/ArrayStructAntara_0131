1bac5d226dc9a8b2fb0d0d92b58657fb51a80cac
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-07-23 14:06:37 +0700

    perbaikan akses dan modifikasi elemen array

--- array.cpp
+++ array.cpp
@@ -7,4 +7,10 @@ int main()
     int dataku[5] = [10, 20, 30, 40, 50];
     //Deklarasi Tanpa inisialisasi
     int datamu[5];
+
+    //Cara akses (membaca atau mengambil isi array)
+    cout << "isi data kedua : " <<  dataku[1] << endl;
+    //Mengisi error
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
 }
